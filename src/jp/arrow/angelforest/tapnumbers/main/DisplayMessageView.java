8dd07307cadfa511d01ce8b7911c088ae5b20e27
horihori <horikawa_yoji@futurescope.jp>
2010-07-20 12:18:04 +0900

    conflict test from master

--- src/jp/arrow/angelforest/tapnumbers/main/DisplayMessageView.java
+++ src/jp/arrow/angelforest/tapnumbers/main/DisplayMessageView.java
@@ -99,8 +99,8 @@ public class DisplayMessageView extends View {
 
     private void displayGo(Canvas canvas) {
         paint.setColor(Color.CYAN);
+        //Conflict test
         canvas.drawText("Go! " + counter, 0, 10, paint);
-//        Log.e(null, "Go: " + counter);
         counter++;
     }
 
